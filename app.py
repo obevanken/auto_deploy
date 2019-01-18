3ca3e9468f52ed31e03e1311d43d688eb316eb60
Sergey Fedorov <blueberrycat@192.168.1.62.ipl.orionnet.ru>
2019-01-18 17:21:39 +0700

    fix

--- app.py
+++ app.py
@@ -1,36 +1,49 @@
 from flask import Flask, request, abort
-import os
 import json
 import logging
 import subprocess
 
-
 logging.basicConfig(level="INFO")
 application = Flask(__name__)
+
 
 @application.route('/push', methods=['POST'])
 def push_event():
     data = request.get_json()
+
+    # Headers
+    event = request.headers.get('X-Gitlab-Event')
+    token = request.headers.get('X-Gitlab-Token')
+
+    # Data
     branch = data["ref"].split('/')[2]
     repo_name = data['repository']["name"]
-    with open('labhook.json') as f:
+    project_id = data['project']['id']
+
+    with open('repo_settings.json') as f:
         file_data = json.load(f)
-    
+
+    if file_data['token'] != token or event not in file_data['events']:
+        logging.warning('invalid token or forbidden event')
+        return abort(500)
+
     if file_data['repo_name'] != repo_name:
         logging.warning('repositories do not match, check "repo_name" in file')
         return abort(500)
 
-    if branch == file_data['branch']:
-        path = file_data['path']
-        os.chdir(path)
-        if not file_data['commands']: 
-            logging.warning("Commands is empty") 
-            return abort(500)
-        for command in file_data['commands']:
-            subprocess.check_call(command, shell=True)
-    else:
-        logging.warning("Not this branch")
+    if file_data['project_id'] != project_id:
+        logging.warning('invalid project_id')
         return abort(500)
-    
-    logging.info("OK")  
+
+    if file_data['branch'] != branch:
+        logging.warning('invalid branch')
+        return abort(500)
+
+    logging.info("OK")
+
+    process = subprocess.call(file_data['path'])
+    for line in process.stdout:
+        print(line.decode().strip())
+
     return 'OK', 200
+#
